ac9f32a1211f9c864cdc85b9cb377e5f7c870b7b
Jeremy BOURDIOL <jerem.dante@gmail.com>
2014-02-22 18:06:01 +0100

    2 Windows
    Now works with 1 component per window

--- src/projetjeudes15/ProjetJeuDes15.java
+++ src/projetjeudes15/ProjetJeuDes15.java
@@ -4,6 +4,12 @@
  */
 package projetjeudes15;
 
+import java.awt.Dimension;
+import javax.swing.JFrame;
+import projetjeudes15.graphic_components.GridDisplay;
+import projetjeudes15.graphic_components.ListDisplay;
+import projetjeudes15.models.Jeu15Model;
+
 /**
  *
  * @author bourdije
@@ -14,6 +20,21 @@ public class ProjetJeuDes15 {
      * @param args the command line arguments
      */
     public static void main(String[] args) {
-        // TODO code application logic here
+        JFrame grid = new JFrame();
+        Jeu15Model model = new Jeu15Model(2);
+        GridDisplay g = new GridDisplay();
+        g.setModel(model);
+        grid.add(g);
+        grid.setVisible(true);
+        grid.setSize(new Dimension(300, 300));
+        grid.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        
+        JFrame list = new JFrame();
+        ListDisplay l = new ListDisplay();
+        l.setModel(model);
+        list.add(l);
+        list.setVisible(true);
+        list.setSize(new Dimension(600, 300));
+        list.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
     }
 }
